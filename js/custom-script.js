c52d720c478660b62647dc7e2b5649fad81b40bc
red2cracker <red2cracker@gmail.com>
2019-08-13 10:57:25 +0900

    자바스크립트에서 리사이즈시 offset 값 변화.

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,15 +1,20 @@
 $(function(){
 
 /* https://www.codingfactory.net/10745 */
-var navbarOffset = $( '.navbar' ).offset();
+var navbarOffset = $('.navbar').offset();
 //alert(navbarOffset.top);
 
-$( window ).scroll(function() {
-  if ( $( document ).scrollTop() > navbarOffset.top ) {
-    $( '.navbar' ).addClass( 'navbar-fixed' );
+/* https://sir.kr/qa/318793 */
+$(window).resize(function() {
+  navbarOffset = $('.navbar').offset();
+});
+
+$(window).scroll(function() {
+  if ( $(document).scrollTop() > navbarOffset.top ) {
+    $('.navbar').addClass('navbar-fixed');
   }
   else {
-    $( '.navbar' ).removeClass( 'navbar-fixed' );
+    $('.navbar').removeClass('navbar-fixed');
   }
 });
 
